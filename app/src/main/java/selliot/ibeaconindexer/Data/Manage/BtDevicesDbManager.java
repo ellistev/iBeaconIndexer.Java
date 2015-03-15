b0767347aabab840fbcb9f2f3365c69e4e547a61
Steven Elliott <steven.peter.elliott@gmail.com>
2015-03-15 15:10:55 -0600

    added update and delete methods

--- app/src/main/java/selliot/ibeaconindexer/Data/Manage/BtDevicesDbManager.java
+++ app/src/main/java/selliot/ibeaconindexer/Data/Manage/BtDevicesDbManager.java
@@ -1,5 +1,6 @@
 package selliot.ibeaconindexer.Data.Manage;
 
+import android.bluetooth.BluetoothClass;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -39,6 +40,24 @@ public class BtDevicesDbManager {
                 values);
     }
 
+    public void UpdateBtDevice(BtDevice deviceToUpdate) {
+        SQLiteDatabase db = btDeviceDbHelper.getReadableDatabase();
+
+        // New value for one column
+        ContentValues values = new ContentValues();
+        values.put(BtDeviceContract.BtDevicesTable.COLUMN_NAME_NAME, deviceToUpdate.Name);
+
+        // Which row to update, based on the ID
+        String selection = BtDeviceContract.BtDevicesTable.COLUMN_NAME_UUID + " LIKE ?";
+        String[] selectionArgs = {deviceToUpdate.UuidString};
+
+        int count = db.update(
+                BtDeviceContract.BtDevicesTable.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+    }
+
     public void GetBtDevice(String uuidToFind){
         SQLiteDatabase db = btDeviceDbHelper.getReadableDatabase();
 
@@ -67,13 +86,15 @@ public class BtDevicesDbManager {
         );
     }
 
+
     public void DeleteBtDevice(int rowId){
+        SQLiteDatabase db = btDeviceDbHelper.getWritableDatabase();
         // Define 'where' part of query.
         String selection = BtDeviceContract.BtDevicesTable.COLUMN_NAME_ID + " LIKE ?";
 // Specify arguments in placeholder order.
         String[] selectionArgs = { String.valueOf(rowId) };
 // Issue SQL statement.
-        db.delete(table_name, selection, selectionArgs);
+        db.delete(BtDeviceContract.BtDevicesTable.TABLE_NAME, selection, selectionArgs);
     }
 
 }
